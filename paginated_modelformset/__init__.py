53cee7521d9db8fa6b650996b23389035cfac16a
Alex Parinov <creafz@gmail.com>
2014-12-10 14:33:15 +0400

    Init paginated_modelformset package

---
+++ paginated_modelformset/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from paginated_modelformset.formset import PaginatedModelFormSet
+from paginated_modelformset.paginator import FormSetPage, FormSetPaginator
+
+
+__author__ = 'Alex Parinov'
+__email__ = 'creafz@gmail.com'
+__license__ = 'MIT'
+__version__ = '0.0.1'
+
+__all__ = [
+    'FormSetPage',
+    'FormSetPaginator',
+    'PaginatedModelFormSet'
+]
