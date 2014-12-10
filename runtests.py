fc4ac1290e148058f710244d7d99f72038f31459
Alex Parinov <creafz@gmail.com>
2014-12-10 14:37:03 +0400

    Add runtests file

---
+++ runtests.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+import os
+
+import django
+from django.conf import settings
+from django.core.management import call_command
+
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+
+settings.configure(
+    INSTALLED_APPS=(
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.sites',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'paginated_modelformset.tests',
+    ),
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':MEMORY:',
+        }
+    },
+    MIDDLEWARE_CLASSES=(
+        'django.middleware.common.CommonMiddleware',
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+    ),
+)
+
+# to support Django >= 1.7
+if django.VERSION[:2] >= (1, 7):
+    django.setup()
+
+
+if __name__ == "__main__":
+    call_command('test', 'paginated_modelformset.tests')
